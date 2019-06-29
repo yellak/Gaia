2150951611c322c78718751f08e8b9cf0922bef7
Kálley Wilkerson <yellakkalley@gmail.com>
2019-06-29 20:55:39 -0300

    Gaia pronta para ser apresentada

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include "uart.h"
 #include "stdio.h"
 #define THRESHOLD 0x700
+#define GOOD_FOR_LIFE 80
+#define WAIT 30
 
 uint8_t updateDisplay = 0;
 uint32_t media = 0;
@@ -10,8 +12,10 @@ uint32_t results[15] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 uint8_t count = 0;
 uint8_t actual = 0;
 uint8_t i;
+uint32_t count2Sec = 0;
+uint8_t watering = 0;
 char moisture[11];
-float umidade;
+float umidade, umidadeMedia;
 volatile float factor;
 
 void UpdateMedia() {
@@ -25,9 +29,31 @@ void UpdateMedia() {
     if(media < THRESHOLD) {
         media = THRESHOLD;
     }
-    umidade = 100 - (media - THRESHOLD)*factor;
-    sprintf(moisture, "%.1f%%   ", umidade);
+    umidadeMedia = 100 - (media - THRESHOLD)*factor;
+    sprintf(moisture, "%.1f%%   ", umidadeMedia);
     lcdWriteStr(moisture);
+    uartSendStr("------------------------------\n");
+    uartSendStr("Media: ");
+    uartSendStr(moisture);
+    uartSendStr("\n------------------------------\n");
+
+    if(count2Sec >= WAIT) {
+        count2Sec = 0;
+        if(umidadeMedia < GOOD_FOR_LIFE) {
+            P1OUT |= BIT1; // led verde
+            P4OUT &= ~BIT4;
+            watering = 1;
+            // Habilitando contagem de 1 segundo e interrupção de rega
+            TA1CTL |= MC__UP | TACLR;
+            TA1CCTL0 |= CCIE;
+            uartSendStr("\nRegando...\n");
+            while(watering == 1);
+            uartSendStr("Planta molhada :D\n\n");
+        }
+        else {
+            uartSendStr("Umidade suficiente.\n");
+        }
+    }
 }
 
 void updateHumidity() {
@@ -52,7 +78,12 @@ int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	PM5CTL0 &= ~LOCKLPM5;
-	P1DIR |= BIT0;
+	P1DIR |= (BIT0 | BIT1);
+
+	// Mantendo a rega desligada
+	P4DIR |= BIT4;
+	P4OUT |= BIT4;
+	P1OUT &= ~BIT1;
 	
     // Configurando o canal A12
     adc12ConfChannelA12();
@@ -62,10 +93,14 @@ int main(void)
     LCD_init();
 
     // Timer para controlar o conversor AD
+    // PWM
     TA0CTL = TASSEL__ACLK | MC__UP | TACLR;
     TA0CCTL1 |= OUTMOD_7;
     TA0CCR0 = 65535;
     TA0CCR1 = 50;
+
+    TA1CTL = TASSEL__ACLK | TACLR;
+    TA1CCR0 = 65535;
     ADC12CTL0 |= ADC12ENC;
 
     // Inicializano modulo bluetooth
@@ -114,5 +149,15 @@ __interrupt void ADC12_ISR() {
     results[count] = (uint32_t)(med/16);
     updateDisplay = 1;
     P1OUT ^= BIT0;
+    count2Sec++;
     ADC12CTL0 |= ADC12ENC;
+}
+
+#pragma vector = TIMER1_A0_VECTOR
+__interrupt void TA1CCR0_ISR() {
+    P1OUT &= ~BIT1;
+    P4OUT |= BIT4;
+    watering = 0;
+    TA1CCTL0 &= ~CCIE;
+    TA1CTL &= ~MC__UP;
 }
